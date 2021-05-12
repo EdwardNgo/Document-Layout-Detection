a0ce876247202d9a46225fc9e1db7a8170b294cf
edwardngo <hoangso8000@gmail.com>
2021-05-12 12:57:10 +0700

    Add: install package by bash

--- infer.py
+++ infer.py
@@ -2,14 +2,9 @@
 import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
-
 # import some common libraries
 import numpy as np
 import cv2
-import random
-# from google.colab.patches import cv2_imshow
-
-# import some common detectron2 utilities
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
@@ -18,6 +13,9 @@ from detectron2.data import MetadataCatalog
 from detectron2.engine import DefaultTrainer
 import os
 from detectron2.evaluation import COCOEvaluator
+import logging
+from detectron2.utils.visualizer import ColorMode
+
 
 class CocoTrainer(DefaultTrainer):
 
@@ -29,50 +27,40 @@ class CocoTrainer(DefaultTrainer):
         output_folder = "coco_eval"
 
     return COCOEvaluator(dataset_name, cfg, False, output_folder)
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")) #Get the basic model configuration from the model zoo 
 
-# # Number of data loading threads
-# cfg.DATALOADER.NUM_WORKERS = 4
-# # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-# # Number of images per batch across all machines.
-# cfg.SOLVER.IMS_PER_BATCH = 4
-# cfg.SOLVER.BASE_LR = 0.0125  # pick a good LearningRate
-# cfg.SOLVER.MAX_ITER = 1500  #No. of iterations   
-# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
-# cfg.TEST.EVAL_PERIOD = 500 # No. of iterations after which the Validation Set is evaluated. 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+def inference(img_path,save_path,device = "cpu"):
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")) #Get the basic model configuration from the model zoo 
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
-from detectron2.utils.visualizer import ColorMode
+    #Use the final weights generated after successful training for inference  
+    cfg.MODEL.WEIGHTS = "model/model_final.pth"
+    cfg.MODEL.DEVICE = "cpu"
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8  # set the testing threshold for this model
+    #Pass the validation dataset
+    # cfg.DATASETS.TEST = ("pubdal6_val", )
+    cfg.DATASETS.TRAIN = ("pubdal6_train",)
+    cfg.DATASETS.TEST = ("pubdal6_val",)
+    predictor = DefaultPredictor(cfg)
 
-#Use the final weights generated after successful training for inference  
-cfg.MODEL.WEIGHTS = "/home/viethoang/petproject/20202/Document_Layout_Analysis/model/model_final.pth"
-cfg.MODEL.DEVICE = "cpu"
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8  # set the testing threshold for this model
-#Pass the validation dataset
-# cfg.DATASETS.TEST = ("pubdal6_val", )
-cfg.DATASETS.TRAIN = ("pubdal6_train",)
-cfg.DATASETS.TEST = ("pubdal6_val",)
-predictor = DefaultPredictor(cfg)
+    # dataset_dicts = get_board_dicts("/content/val","val.json")
+    # for d in random.sample(dataset_dicts, 10):    
+    im = cv2.imread(img_path)
+    outputs = predictor(im)
+    print(outputs)
+    board_metadata = MetadataCatalog.get("pubdal6_train").set(thing_classes=["Text","Title","List","Table","Figure"])
+    v = Visualizer(im[:, :, ::-1],
+                    metadata=board_metadata, 
+                    scale=1,
+                    instance_mode=ColorMode.IMAGE   
+    )
+    # from google.colab.patches import cv2_imshow
+    import numpy as np
+    from PIL import Image
 
-# dataset_dicts = get_board_dicts("/content/val","val.json")
-# for d in random.sample(dataset_dicts, 10):    
-im = cv2.imread("data/PMC3976938_00002.jpg")
-outputs = predictor(im)
-print(outputs)
-board_metadata = MetadataCatalog.get("pubdal6_train").set(thing_classes=["Text","Title","List","Table","Figure"])
-v = Visualizer(im[:, :, ::-1],
-                metadata=board_metadata, 
-                scale=0.8,
-                instance_mode=ColorMode.IMAGE   
-)
-# from google.colab.patches import cv2_imshow
-import numpy as np
-from PIL import Image
-
-v = v.draw_instance_predictions(outputs["instances"].to("cpu")) #Passing the 
-data = v.get_image()[:, :, ::-1]
-img = Image.fromarray(data, 'RGB')
-img.save('annotate/PMC3976938_00002.jpg')
-img.show()
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu")) #Passing the 
+    data = v.get_image()[:, :, ::-1]
+    img = Image.fromarray(data, 'RGB')
+    img.save(save_path +  "/" + img_path.split("/")[-1])
+  # img.show()
