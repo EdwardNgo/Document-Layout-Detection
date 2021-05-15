1bfe3861d613b6b5bbad92c910d20663ac4157fc
edwardngo <hoangso8000@gmail.com>
2021-05-15 21:21:37 +0700

    Test connection

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+pip install wtf
 pip install -U torch==1.5 torchvision==0.6 -f https://download.pytorch.org/whl/cu101/torch_stable.html
 pip install cython pyyaml==5.1
 pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
